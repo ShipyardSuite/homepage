cbebcce23d3abce28ea0470a85b5ab8a72082811
Frederik Reich <frederikreich@googlemail.com>
2020-03-19 17:01:06 +0100

    Initial commit

---
+++ app/initialize.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import App from './App';
+
+import { Home, NotFound } from './containers';
+
+ReactDOM.render(
+	<Router>
+		<App>
+			<Switch>
+				<Route exact path="/template/" component={Home} />
+				<Route component={NotFound} />
+			</Switch>
+		</App>
+	</Router>,
+	document.querySelector('#root')
+);
