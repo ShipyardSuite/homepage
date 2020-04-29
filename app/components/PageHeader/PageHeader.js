fc9ce91cc7e02b392c93a336b0e92ca9ab3f1184
Frederik Reich <frederikreich@googlemail.com>
2020-04-29 20:57:20 +0200

    Add new files

--- app/components/PageHeader/PageHeader.js
+++ app/components/PageHeader/PageHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Menu, Button } from 'semantic-ui-react';
+import { Segment, Container, Menu, Icon, Image } from 'semantic-ui-react';
 
 /**
  * Default class for react Application
@@ -13,24 +13,37 @@ export default class PageHeader extends React.Component {
 	 */
 	render() {
 		return (
-			<Menu secondary>
-				<Menu.Item disabled>Features</Menu.Item>
-				<Menu.Item name="documentation" href={`http://${window.location.host}/documentation/`}>
-					Documentation
-				</Menu.Item>
-				<Menu.Menu position="right">
-					<Menu.Item name="help">
-						<Menu.Item name="login" href={`http://${window.location.host}/auth/login`}>
-							Login
+			<Segment inverted style={{ borderRadius: 0 }}>
+				<Container fluid>
+					<Menu stackable inverted secondary>
+						<Menu.Item>
+							{/**
+							 * @todo Fix image link
+							 * @body The header image path needs to be fixed.
+							 */}
+							<Image src="./public/images/shipyard_logo_icon.png" size="mini" />
 						</Menu.Item>
-					</Menu.Item>
-				</Menu.Menu>
-			</Menu>
+						{/* <Menu.Item disabled>FEATURES</Menu.Item> */}
+						<Menu.Item href={`http://${window.location.host}/documentation/`}>DOCUMENTATION</Menu.Item>
+						<Menu.Menu position="right">
+							{/**
+							 * @todo Fix semantic-ui button
+							 * @body The Button element does not work currently, so only it's css has been used, use the real Button element instead.
+							 */}
+							<div className="item">
+								<a className="ui primary button" href={`http://${window.location.host}/auth/login`}>
+									Dashboard
+								</a>
+							</div>
+							<Menu.Item>
+								<a href="https://github.com/ShipyardSuite">
+									<Icon name="github" size="big" />
+								</a>
+							</Menu.Item>
+						</Menu.Menu>
+					</Menu>
+				</Container>
+			</Segment>
 		);
 	}
 }
-/*
-				<Menu.Item href={`http://${window.location.host}`}>
-					<img src="https://react.semantic-ui.com/logo.png" />
-				</Menu.Item>
-*/
